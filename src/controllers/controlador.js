bfac7c04c00b29f1ef1009b329aa2b7129d6be95
FelipeVelezG <107275484+FelipeVelezG@users.noreply.github.com>
2022-07-21 17:45:15 -0500

    Se modifica el index y se crea controlador js

---
+++ src/controllers/controlador.js
@@ -0,0 +1,11 @@
+// Creando variables
+
+let nombreUsuario = "Felipe Velez"
+const EDAD_USUARIO = 32
+let estaturaUsuario = 1.78
+let hinchadaDelVeerde = true
+
+console.log (nombreUsuario)
+
+
+console.log(EDAD_USUARIO)
