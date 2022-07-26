742949b93d2438d16e868f162929a0ef31913c33
FelipeVelezG <107275484+FelipeVelezG@users.noreply.github.com>
2022-07-26 17:36:13 -0500

    se crean ejemplos de DOM,POO y Arrays

---
+++ src/controllers/aprendiendoDOM.js
@@ -0,0 +1,12 @@
+//pasos para controlar una etiqueta desde js
+
+//1.1 Controlando contenido
+let etiquetaTitulo = document.getElementById("titulo")
+
+let etiquetaImagen = document.getElementById("foto")
+
+//1.1 Manipulando eltexto de la etiqueta 
+
+etiquetaTitulo.textContent = " Jefe vacaiones "
+etiquetaImagen.src = "https://firebasestorage.googleapis.com/v0/b/tiendasurajfv.appspot.com/o/Carama%C3%B1olasKarate.jpg?alt=media&token=f5eb3d94-9fa1-4fc4-a752-cba59354b4b8"
+
--- src/controllers/controlador.js
+++ src/controllers/controlador.js
@@ -9,3 +9,37 @@ console.log (nombreUsuario)
 
 
 console.log(EDAD_USUARIO)
+
+
+
+// Areglos de datos
+// Un arreglo es una vatiable especial 
+ //1. Arreglo tradicional 
+let numeros = [1,2,8,9,45]
+let nombres = ["juan","catalina","mario","carlos"]
+let tieneVirusDelMono=[true,false,false,false,true]
+//1.1Accediendo a todos los controladores (elementos) de un arreglo 
+console.log(numeros)
+// 1.2Accediendo a un solo elemento del arreglo 
+console.log(numeros[3])
+
+//2.Areglos orientados a objetos ARREGLO POO
+let notas = Array(3.5,2.8,3.9,5)
+console.log(notas)
+
+
+// 3. Variables especiales para guardar varios datos 
+//de tipos diferentes
+// Objetos 
+let persona = {
+
+    nombre:"Juan Jose",
+    apellidos : ["Gallego","Mesa"],
+    edad : 33,
+    hinchaDelMejor : true
+
+}
+//3.1 Accediendo a todos los datos de un objeto 
+console.log(persona)
+// 3.1 Accediendo a un solo atributo del objeto 
+console.log(persona.nombre)
